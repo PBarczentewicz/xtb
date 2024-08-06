050348b4a130d405e45a6ed9b5ce3a4cbd8ee29d
Przemek <p.barczentewicz@gmail.com>
2024-08-06 12:49:41 +0200

    Test Backend from object

---
+++ src/test/java/ProjectData/WebData.java
@@ -0,0 +1,29 @@
+package ProjectData;
+
+public class WebData {
+    String name;
+    String email;
+    String phone;
+    String subject;
+    String message;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
--- src/test/java/com/example/piotrkrupa/test/BeTest.java
+++ src/test/java/com/example/piotrkrupa/test/BeTest.java
@@ -1,15 +1,14 @@
 package com.example.piotrkrupa.test;
 
+import ProjectData.WebData;
 import com.example.piotrkrupa.base.BaseTest;
 import com.example.piotrkrupa.service.GetService;
 import io.restassured.http.ContentType;
-import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import javax.json.Json;
 import java.io.File;
-import java.text.ParsePosition;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,7 +19,7 @@ public class BeTest extends BaseTest {
     @Test
     @DisplayName("TestBE")
     public void Send() throws InterruptedException {
-        File newPost = new File("com/example/piotrkrupa/test/PostTest.json");
+        File newPost = new File("src/test/java/com.example.piotrkrupa/test/PostTest.json");
 
         given().log().all().contentType(ContentType.JSON).body(newPost)
                 .when().post("https://automationintesting.online")
@@ -39,21 +38,38 @@ public class BeTest extends BaseTest {
                 "}";
 
         given().log().all().contentType(ContentType.JSON).body(newPost)
-                .when().post("https://automationintesting.online/message/")
+                .when().post("https://automationintesting.online")
                 .then().log().all();
     }
 
+
+//    @Test
+//    public void addPostFromMap() {
+//        Map<String, Object> newPost = new HashMap<>();
+//        newPost.put("Name", "Tomas");
+//        newPost.put("Email", "TOmasowy");
+//        newPost.put("Phone", "987654321");
+//        newPost.put("Subject", "Subject Test");
+//        newPost.put("Message", "Message test");
+//
+//        given().log().all().contentType(ContentType.JSON).body(newPost)
+//                .when().post("104.21.25.4")
+//                .then().log().all();
+//    }
+
+
     @Test
-    public void addPostFromMap() {
-        Map<String, Object> newPost = new HashMap<>();
-        newPost.put("Name", "Tomas");
-        newPost.put("Email", "TOmasowy");
-        newPost.put("Phone", "987654321");
-        newPost.put("Subject", "Subject Test");
-        newPost.put("Message", "Message test");
+    public void addPostFromObject(){
+        WebData newPost = new WebData();
+        newPost.setName("Przemyslaw");
+        newPost.setEmail("jsjs@gmail.com");
+        newPost.setPhone("92929283838374");
+        newPost.setSubject("test subject");
+        newPost.setMessage("message test");
+
 
         given().log().all().contentType(ContentType.JSON).body(newPost)
-                .when().post("104.21.25.4")
+                .when().post("https://automationintesting.online")
                 .then().log().all();
     }
 }
