f9579410b9fba61e26dc0afa2c377b046ec87ca6
Przemek <p.barczentewicz@gmail.com>
2024-12-06 15:42:40 +0100

    add parametrized test

--- src/test/java/com/example/xtb/page/HomePage.java
+++ src/test/java/com/example/xtb/page/HomePage.java
@@ -2,6 +2,7 @@ package com.example.xtb.page;
 
 import com.example.xtb.base.BasePage;
 import com.example.xtb.service.Utylities;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -37,13 +38,54 @@ public class HomePage extends BasePage {
         return this;
     }
 
+    public void waitForCompleteWebsite() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        wait.until(ExpectedConditions.urlContains(new Utylities().getExpectedUrl()));
+    }
+
+    public void waitForAllert() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@ng-bind-html='loginErrorMsg']")));
+    }
+
     public void assertLoggin() {
         String curentUrl = driver.getCurrentUrl();
         assertEquals((new Utylities().getExpectedUrl()), curentUrl);
     }
 
-    public void waitForCompleteWebsite() {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-        wait.until(ExpectedConditions.urlContains(new Utylities().getExpectedUrl()));
+    public void assertSuccesLogin() {
+        waitForCompleteWebsite();
+        String curentUrl = driver.getCurrentUrl();
+        assertEquals((new Utylities().getExpectedUrl()), curentUrl);
+    }
+
+    public void assertWrongFillingLoginForm() {
+        String wrongEmail = findByXpath("//div[@ng-bind-html='loginErrorMsg']").getText();
+
+        if (wrongEmail.equals("Nieprawidłowe dane logowania. W adresie e-mail i haśle rozróżniana jest wielkość liter.")) {
+            assertEquals("Nieprawidłowe dane logowania. W adresie e-mail i haśle rozróżniana jest wielkość liter.", wrongEmail);
+        } else if (wrongEmail.equals("Incorrect login credentials. Email and password are case sensitive.")) {
+            assertEquals("Incorrect login credentials. Email and password are case sensitive.", wrongEmail);
+        }
+    }
+
+    public void assertParametrizedTest(String confirmation) {
+        switch (confirmation) {
+            case "success":
+                assertSuccesLogin();
+                break;
+            case "wrongEmail":
+                waitForAllert();
+                assertWrongFillingLoginForm();
+                break;
+            case "wrongEmailWrongPassword":
+                waitForAllert();
+                assertWrongFillingLoginForm();
+                break;
+            case "wrongPassword":
+                waitForAllert();
+                assertWrongFillingLoginForm();
+                break;
+        }
     }
 }
--- src/test/java/com/example/xtb/test/LoginTest.java
+++ src/test/java/com/example/xtb/test/LoginTest.java
@@ -3,11 +3,12 @@ package com.example.xtb.test;
 import com.example.xtb.service.Utylities;
 import com.example.xtb.base.BaseTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class LoginTest extends BaseTest {
-
     @Test
-    public void login(){
+    public void login() {
         navigateToHomePage();
         Utylities utylities = new Utylities();
 
@@ -17,5 +18,20 @@ public class LoginTest extends BaseTest {
 
         homePage.waitForCompleteWebsite();
         homePage.assertLoggin();
+    }
+    @ParameterizedTest
+    @CsvSource({
+            "p.barczentewicz@gmail.com, HasloTestowe1@, success",
+            "testowy@mail.pl,HasloTestowe1@, wrongEmail",
+            "testowy@mail.pl, WrongPassword, wrongEmailWrongPassword",
+            "p.barczentewicz@gmail.com, WrongPassword, wrongPassword",
+    })
+    public void loginWithParametrized(String email, String password, String confirmation) {
+        navigateToHomePage();
+        homePage.setEmail(email)
+                .setPassword(password)
+                .clickLoginButton();
+
+        homePage.assertParametrizedTest(confirmation);
     }
 }
